77ab3bda4664a40ba7ad4690c44b7c431f8799b4
Dawe <david.zhenetl@gmail.com>
2022-10-13 19:54:38 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            printTable(out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printTable(OutputStream out) throws IOException {
+        for (int i = 1; i < 10; i++) {
+            out.write(("1 * " + i + " = " + (1 * i) + System.lineSeparator()).getBytes());
+        }
+    }
+}
