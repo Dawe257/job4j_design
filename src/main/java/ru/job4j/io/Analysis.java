ae398015965de6722b06268bb41b48c88e3a9439
Dawe <david.zhenetl@gmail.com>
2022-10-13 21:36:09 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public static void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
+            boolean down = false;
+            StringBuilder downTime = new StringBuilder();
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String status = line.split(" ")[0];
+                String time = line.split(" ")[1];
+                if (!status.equals("200") && !down) {
+                    down = true;
+                    downTime.append(time).append(";");
+                } else if (status.equals("200") && down) {
+                    down = false;
+                    downTime.append(time).append(";");
+                    out.write(downTime + System.lineSeparator());
+                    downTime = new StringBuilder();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        unavailable("server.log", "result.log");
+    }
+}
