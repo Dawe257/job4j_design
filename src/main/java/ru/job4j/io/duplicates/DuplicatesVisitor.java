11fb4af10df36ae1c17b5dcb5717b6e58e0d37a8
dzhenetl <zhenetldm@it.mos.ru>
2022-10-27 14:59:40 +0300

    4.2. Поиск дубликатов [#315066 #355416]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -9,26 +9,20 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final List<FileProperty> visitedFiles = new ArrayList<>();
     private final Map<FileProperty, List<Path>> visitedFilesPaths = new HashMap<>();
 
-        @Override
+    @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
-            if (!visitedFiles.contains(fileProperty)) {
-                visitedFiles.add(fileProperty);
-                visitedFilesPaths.put(fileProperty, new ArrayList<>());
-                visitedFilesPaths.get(fileProperty).add(file);
-            } else {
-                visitedFilesPaths.get(fileProperty).add(file);
-            }
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        visitedFilesPaths.putIfAbsent(fileProperty, new ArrayList<>());
+        visitedFilesPaths.get(fileProperty).add(file);
         return super.visitFile(file, attrs);
     }
 
     public Map<FileProperty, List<Path>> getDuplicates() {
-            return visitedFilesPaths.entrySet()
-                    .stream()
-                    .filter(entry -> entry.getValue().size() > 1)
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        return visitedFilesPaths.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
