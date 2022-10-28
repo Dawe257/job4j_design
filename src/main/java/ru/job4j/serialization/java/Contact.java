3d9cb881cceff4ef32a96fef2fe261d6430890fe
dzhenetl <zhenetldm@it.mos.ru>
2022-10-28 16:09:30 +0300

    1. Что такое Сериализация? [#313163]

---
+++ src/main/java/ru/job4j/serialization/java/Contact.java
@@ -0,0 +1,61 @@
+package ru.job4j.serialization.java;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int zipCode;
+    private final String phone;
+
+    public Contact(int zipCode, String phone) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "zipCode=" + zipCode
+                + ", phone='" + phone + '\''
+                + '}';
+    }
+
+    public boolean equals(Contact c) {
+        return this.zipCode == c.zipCode && this.phone.equals(c.phone);
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
+
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             ObjectOutputStream oos =
+                     new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+        }
+
+        final Contact contactFromFile;
+        try (FileInputStream fis = new FileInputStream(tempFile);
+             ObjectInputStream ois =
+                     new ObjectInputStream(fis)) {
+            contactFromFile = (Contact) ois.readObject();
+        }
+
+        Logger logger = LoggerFactory.getLogger(Contact.class.getName());
+        logger.info(contact.toString());
+        logger.info(contactFromFile.toString());
+        logger.info(String.valueOf(contact.equals(contactFromFile)));
+    }
+}
