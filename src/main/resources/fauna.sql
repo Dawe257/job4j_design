0a99f562b0c8cae154c63ef84c6b9ffaeb70cd24
dzhenetl <david.zhenetl@gmail.com>
2022-11-11 09:25:10 +0300

    0. Select [#6861 #359606]

--- src/main/resources/fauna.sql
+++ src/main/resources/fauna.sql
@@ -1,7 +1,33 @@
-select * from fauna where name like '%fish%';
+create table fauna
+(
+    id             serial primary key,
+    name           varchar(255),
+    avg_age        int,
+    discovery_date date
+);
 
-select * from fauna where avg_age >= 10000 and avg_age <= 21000;
+insert into fauna(name, avg_age, discovery_date)
+values ('fish', 10000, '1000-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('cow', 15000, '2000-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('dog', 25000, '3000-01-01');
+insert into fauna(name, avg_age)
+values ('cat', 20000);
 
-select * from fauna where discovery_date IS NULL;
+select *
+from fauna
+where name like '%fish%';
 
-select * from fauna where discovery_date < '1950-01-01';
+select *
+from fauna
+where avg_age >= 10000
+  and avg_age <= 21000;
+
+select *
+from fauna
+where discovery_date IS NULL;
+
+select *
+from fauna
+where discovery_date < '1950-01-01';
