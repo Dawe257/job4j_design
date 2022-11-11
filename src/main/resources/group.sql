634925277501d04fa8e2bcf57d1d7ff02f92de22
dzhenetl <david.zhenetl@gmail.com>
2022-11-11 08:29:32 +0300

    2. Группировка и агрегатные функции [#373618 #359621]

--- src/main/resources/group.sql
+++ src/main/resources/group.sql
@@ -1,3 +1,36 @@
+create table devices
+(
+    id    serial primary key,
+    name  varchar(255),
+    price float
+);
+
+create table people
+(
+    id   serial primary key,
+    name varchar(255)
+);
+
+create table devices_people
+(
+    id        serial primary key,
+    device_id int references devices (id),
+    people_id int references people (id)
+);
+
+insert into devices(name, price) values ('iphon', 10000);
+insert into devices(name, price) values ('sasung', 8000);
+insert into devices(name, price) values ('ximi', 4000);
+
+insert into people(name) values ('петя');
+insert into people(name) values ('ваня');
+insert into people(name) values ('степа');
+
+insert into devices_people(device_id, people_id) values (1, 1);
+insert into devices_people(device_id, people_id) values (2, 2);
+insert into devices_people(device_id, people_id) values (3, 3);
+insert into devices_people(device_id, people_id) values (1, 3);
+
 select avg(price)
 from devices;
 
