62b754a29d1f104e97627cd87f073063d24a585f
dzhenetl <david.zhenetl@gmail.com>
2023-01-31 12:00:52 +0300

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class GeneratorTest {
+
+    private Generator generator;
+
+    @Test
+    void produce() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        String expected = "I am a Petr Arsentev, Who are you?";
+        String actual = generator.produce(template, args);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldThrowExceptionWithWrongGeyInTemplate() {
+        String template = "I am a ${name}, Who ${wrongKey} are ${subject}?";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+    }
+
+    @Test
+    void shouldThrowExceptionWithWrongGeyInArgs() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you", "wrongKey", "");
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+    }
+}
